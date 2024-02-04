d3c4cfeb7f128eb2ee79e2ba64337d2383e2ebde
Rodrigo Dias <roycocup@users.noreply.github.com>
2024-02-04 09:17:24 +0000

    Update global.py

--- global.py
+++ global.py
@@ -1,14 +1,13 @@
-#!/usr/local/bin/python3
-
-import glob
+#!/usr/bin/python3
 import os
-import subprocess
+import glob
 
-files = glob.glob('*.mkv')
-for fil in files:
-    cmd = ["ffmpeg","-i",fil,"-vf","scale=1280x720:flags=bilinear","-c:v","libx264","-preset","ultrafast","-crf","23",fil+".mp4"]
-    subprocess.call(cmd)
-    cmd = ['rm', fil]
-    subprocess.call(cmd)
-    
+filenames = glob.glob("*.mp4")
+for filename in filenames:
+        cmd = []
+        cmd.append(f"nice -n15 ffmpeg -i {filename} -vcodec libx265 -crf 20 comp.{filename} && ")
+        cmd.append(f"mv comp.{filename} shortened/{filename} && ")
+        cmd.append(f'rm -rf {filename}')
+       
+        os.system("".join(cmd))
 
